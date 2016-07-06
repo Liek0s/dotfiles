ec9e0d168f9f7f5018348109b2d59da3303d9936
Pierre Jackman <pierre.jackman@coopengo.com>
2016-07-06 16:40:45 +0200

    Updated emacs

---
+++ emacs/emacs.d/elpa/git-gutter-fringe-20160520.1656/git-gutter-fringe-autoloads.el
@@ -0,0 +1,16 @@
+;;; git-gutter-fringe-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("git-gutter-fringe.el") (22395 26417 420100
+;;;;;;  484000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; git-gutter-fringe-autoloads.el ends here
