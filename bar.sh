d8ca6654164b76e03842c94c74ecb0a01dcfd02c
Jack <jackman.pierre@gmail.com>
2017-02-26 17:03:48 +0000

    update lemonbar script

--- bar.sh
+++ bar.sh
@@ -1,100 +1,134 @@
 #!/bin/bash
+## Settings ##
 
-# OPTIONS
-WIDTH=""
-HEIGHT="24"
+fifo="/tmp/panel.fifo"
 
-BDWIDTH="5"
+dimensions="x24"
+fg_color="#FFFFFF"
+bg_color="#1D1D1D"
+font="ProFont"
+icon_font="-Wuncon-Siji-Medium-R-Normal--10-100-75-75-C-80-ISO10646-1"
+font_size="10"
 
-BGCOLOR="#1D1D1D"
-FGCOLOR="#FFAC00"
-BDCOLOR="#D89200"
+##########
+## Init ##
+##########
 
-# functions
+if [ "$(pgrep -cx lemonbar)" -gt 0 ]; then
+    printf "%s\n" "Panel is already running." >&2
+    exit 1;
+fi
 
+# TODO - check dependecies
+
+printf "Creating FIFO in %s\n" "$fifo" >&2
+[ -e "$fifo" ] && rm -f "$fifo"
+mkfifo "$fifo"
+
+###############
+## Functions ##
+###############
 get_workspace() {
     glyph="\ue135"
-    ws_name="$(i3-msg -t get_workspaces | python wsbar 2>/dev/null)"
-    echo "%{B#FFAC00}%{F#1D1D1D} $glyph $ws_name  %{B-}%{F-}%{O20}"
+    ws_name=$(i3-msg -t get_workspaces |
+        jq '.[] | select(.focused==true) | .name' |
+        sed -e 's/^"//' -e 's/"$//')
+    echo "workspace:%{B#FFAC00}%{F#000} $glyph $ws_name  %{B-}%{F-}%{O20}"
 }
 
-get_current_song() {
+get_song() {
     glyph="\ue1a6"
-    current_song="$(ncmpcpp --current-song="%t - %a - %b" 2>/dev/null)"
+    current_song=$(ncmpcpp --current-song="%t - %a - %b" 2>/dev/null)
     if [[ ! -z "$current_song" ]]; then
-        echo "$glyph  %{F#FFF}$current_song%{F-}"
+        echo -e "song:%{F#FFAC00}$glyph%{F-} $current_song %{O10}"
     fi
 }
 
 get_network() {
-    glyph="\ue046"
     network="$(iwgetid -r)"
-    if [[ -z "$network" ]]; then
-        echo "\ue21b %{O10}"
-    else
-        echo "$glyph %{F#FFF}$network%{F-}%{O15}" 
-    fi
+    [ -z "$network" ] && glyph="\ue046" || glyph="\ue046"
+    # TODO handle ethernet connection
+    echo -e "network:%{F#FFAC00}$glyph%{F-} $network %{O10}"
 }
 
 get_battery() {
-    current_cap=$(cat /sys/class/power_supply/BAT0/capacity)
+    current=$(cat /sys/class/power_supply/BAT0/capacity)
     status=$(cat /sys/class/power_supply/BAT0/status)
-    if [[ "$status" == "Discharging" ]]; then
-        glyph="\ue20f"
-    else
-        glyph="\ue20e"
-    fi
-    echo "$glyph %{F#FFF}$current_cap%{F-} %{O5}"
+    [ "$status" == "Discharging" ] && glyph="\ue20f" || glyph="\ue20e"
+    echo -e "battery:%{F#FFAC00}$glyph%{F-} $current %{O5}"
 }
 
 get_volume() {
     current_volume=$(amixer get Master |
                             grep -E -o "[0-9]+%" |
                             tail -n 1 | tr -d '%')
-    
-    if [[ ! -z $(amixer get Master | grep -o "off") ]]; then
-        glyph="\ue052"
-    else
-        glyph="\ue050"
-    fi
-    echo "$glyph %{F#FFF}$current_volume%{F-}%{O15}"
-}
-
-get_time() {
-    glyph="\ue0a3"
-    echo "%{F#FFAC00}$glyph %{F#FFF}$(date +"%H:%M:%S")%{F-}%{O15}"
+    is_mute=$(amixer get Master | grep -o "off")
+    [ -z "$is_mute" ] && glyph="\ue050" || glyph="\ue052"
+    echo "volume:%{F#FFAC00}$glyph%{F-} $current_volume%{O15}"
 }
 
 get_date() {
     glyph="\ue225"
-    echo "%{B#FFAC00}%{F#000}  $glyph $(date +"%A, %b %d")  %{B-}"
+    current_date=$(date +"%A, %b %d")
+    echo -e "date:%{B#FFAC00}%{F#000} $current_date $glyph %{B-}%{F-}"
 }
 
-
-draw() {
-    while true; do
-        left="%{l}$(get_workspace)$(get_current_song)"
-        center="%{c}$(get_time)"
-        right="%{r}$(get_battery)$(get_volume)$(get_network)$(get_date)"
-        echo -e "%{S0}$left$right$center%{S1}$left$right$center"
-        sleep 1s;
-    done
+get_time() {
+    glyph="\ue0a3"
+    current_time=$(date +"%H:%M")
+    echo -e "time:%{F#FFAC00}$glyph%{F-} $current_time"
 }
 
-main() {
-    draw | lemonbar -g "${WIDTH}x${HEIGHT}"    \
-                    -B "$BGCOLOR"              \
-                    -F "$FGCOLOR"              \
-                    -U "$BDCOLOR"              \
-                    -u "$BDWIDTH"              \
-                    -f "-Wuncon-Siji-Medium-R-Normal--10-100-75-75-C-80-ISO10646-1" \
-                    -f "ProFont:size=10"
-}
+##################
+## Subprocesses ##
+##################
 
+while :; do get_workspace; sleep 0.5s; done > "$fifo" &
+while :; do get_volume; sleep 1s; done > "$fifo" &
+while :; do get_song; sleep 2s; done > "$fifo" &
+while :; do get_battery; sleep 30s; done > "$fifo" &
+while :; do get_date; sleep 1m; done > "$fifo" &
+while :; do get_time; sleep 30s; done > "$fifo" &
+while :; do get_network; sleep 40s; done > "$fifo" &
 
-if [ $(pgrep -cx $(basename $0)) -gt 1 ] ; then
-    printf "%s\n" "The status bar is already running." >&2
-    exit 1
-fi
+##################
+## Draw the bar ##
+##################
+while read -r line; do
+    case $line in
+        time:*)
+            current_time=${line:5}
+            ;;
+        date:*)
+            current_date=${line:5}
+            ;;
+        volume:*)
+            volume=${line:7}
+            ;;
+        battery:*)
+            battery=${line:8}
+            ;;
+        network:*)
+            network=${line:8}
+            ;;
+        song:*)
+            song=${line:5}
+            ;;
+        workspace:*)
+            workspace=${line:10}
+            ;;
+        *)
+            ;;
+    esac 
+    echo -e \
+        "%{S1}%{l}${workspace}${song}" \
+        "%{c}${current_time}" \
+        "%{r}${battery}${volume}${network}${current_date}"
 
-main
+done < "$fifo" | lemonbar \
+    -g "$dimensions" \
+    -f "$icon_font" \
+    -f "$font:size=$font_size" \
+    -B "$bg_color" \
+    -F "$fg_color" | bash; exit
+
--- dir-locals.el
+++
@@ -1,6 +0,0 @@
-
-
-;; Automatically set PYTHONENV and the virtualenv for project
-((python-mode .
-	      ((eval . (setenv "PYTHONPATH" (concat (projectile-project-root) nil)))
-	       (project-venv-name . nil))))
