f2e1b5b387455fe3b2db00d4a2d099808e9f86cc
Jack <jackman.pierre@gmail.com>
2018-04-17 16:42:25 +0200

    add polybar config and remove unused

--- init.sh
+++ init.sh
@@ -1,15 +1,3 @@
 
 
-init_tmux() {
-
-    TMUX_CONF="$HOME/.tmux.conf"
-    TMUX_CONF_DIR="$HOME/.tmux"
-
-    [ ! -f "$TMUX_CONF_DIR" ] && mkdir -pv "$TMUX_CONF_DIR/plugins"
-
-    git clone "https://github.com/tmux-plugins/tpm.git" "$TMUX_CONF_DIR/plugins/tpm"
-}
-
-init_tmux
-
 
